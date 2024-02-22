538eb60b5c5825f2ab1f3880ef17a04f9ad1eaad
WIRA <s160421134@student.ubaya.ac.id>
2024-02-22 14:27:52 +0700

    Signup comment added

--- app/src/main/java/com/ubaya/anmp_week1/SignupActivity.kt
+++ app/src/main/java/com/ubaya/anmp_week1/SignupActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        ///Sign up bro
+        ///mwgegege
+        ///kapan aku makan ya
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
