959049844e00a9f56dc0815f534b346fa4bb065a
WIRA <s160421134@student.ubaya.ac.id>
2024-03-03 14:13:25 +0700

    Result fragment code

--- app/src/main/java/com/ubaya/anmp_week1/resultFragment.kt
+++ app/src/main/java/com/ubaya/anmp_week1/resultFragment.kt
@@ -5,10 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ubaya.anmp_week1.databinding.FragmentGameBinding
+import com.ubaya.anmp_week1.databinding.FragmentResultBinding
+
 class resultFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private lateinit var binding: FragmentResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        binding = FragmentResultBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    //akses tombol
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(arguments != null) {
+            val score =
+                resultFragmentArgs.fromBundle(requireArguments()).score
+            binding.txtScore.text = "Your score is $score"
+        }
+
+
+        binding.btnMainScreen.setOnClickListener{
+            val action =  resultFragmentDirections.actionMainFragmentResult()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
